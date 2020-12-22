8159590aca122baf2491d8a6c7c87eb3ff3e9bfa
Wisam Mozalbat <wisam_mozalbat@hotmail.com>
2020-12-22 04:28:03 -0400

    created develop branch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
+      {/* side navbar */}
       <div className="header">
         <nav className="navbar">
           <div className="navbar__item">About Me</div>
