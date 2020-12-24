b1b8b9aab6c6dc29a957ae4f457d3fc2e0e963d8
Wisam Mozalbat <wisam_mozalbat@hotmail.com>
2020-12-24 15:22:44 -0400

    Navbar finished for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import './App.css';
+import Header from './layout/Header'
+import { useInView } from 'react-intersection-observer';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [ aboutRef, aboutInView ] = useInView({
+    threshold: 0.3,
+  });
+  const [ skillsRef, skillsInView ] = useInView({
+    threshold: 0.3,
+  });
+  const [ projectsRef, projectsInView ] = useInView({
+    threshold: 0.3,
+  });
+  const [ contactRef, contactInView ] = useInView({
+    threshold: 0.3,
+  });
+
+  const [activeLink, setActiveLink] = useState("about");
+
+  useEffect(() => {
+    if (contactInView) {
+      setActiveLink("contact");
+    } else if (projectsInView) {
+      setActiveLink("projects");
+    } else if (skillsInView) {
+      setActiveLink("skills");
+    } else if (aboutInView) {
+      setActiveLink("about");
+    }
+  }, [aboutInView, skillsInView, projectsInView, contactInView])
+
   return (
     <div className="container">
-      {/* side navbar */}
-      <div className="header">
-        <nav className="navbar">
-          <div className="navbar__item">About Me</div>
-          <div className="navbar__item">Skills</div>
-          <div className="navbar__item">Projects</div>
-          <div className="navbar__item">Contact Me</div>
-        </nav>
-      </div>
+      <Header activeLink={activeLink} />
+      <section className="about" ref={aboutRef} id="about">About Me</section>
+      <section className="skills" ref={skillsRef} id="skills">Skills</section>
+      <section className="projects" ref={projectsRef} id="projects">Projects</section>
+      <section className="contact" ref={contactRef} id="contact">Contact Me</section>
     </div>
   );
 }
---
+++ src/layout/Header.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import './Header.css'
+
+const defaultClasses = {
+    about: ['nav__item'],
+    skills: ['nav__item'],
+    projects: ['nav__item'],
+    contact: ['nav__item']
+}
+
+const Header = props => {
+
+    const [classes, setClasses] = useState({
+        about: ['nav__item'],
+        skills: ['nav__item'],
+        projects: ['nav__item'],
+        contact: ['nav__item']
+    });
+
+    useEffect(() => {
+        let newStyleClasses = {...defaultClasses}
+        for (let item in newStyleClasses) {
+            if (item === props.activeLink) {
+                newStyleClasses[item] = ['nav__item', 'active']
+            } else {
+                newStyleClasses[item] = ['nav__item']
+            }
+        }
+        setClasses({...newStyleClasses})
+    }, [props.activeLink])
+
+    const toggleActive = (activeItem) => {
+        let newStyleClasses = {...classes}
+            for (let item in newStyleClasses) {
+                if (item === activeItem) {
+                    newStyleClasses[item] = ['nav__item', 'active']
+                } else {
+                    newStyleClasses[item] = ['nav__item']
+                }
+            }
+            setClasses({...newStyleClasses})
+    }
+
+    return (
+        <div className="header__container">
+            <div className="header__content">
+                <div className="header__logo">LOGO</div>
+
+                <ul className="nav">
+                    <li className={[classes.about.join(" ")]} onClick={() => toggleActive("about")}>
+                        <a href="#about" className="nav__link">About Me</a></li>
+                    <li className={[classes.skills.join(" ")]} onClick={() => toggleActive("skills")}><a href="#skills" className="nav__link">Skills</a></li>
+                    <li className={[classes.projects.join(" ")]} onClick={() => toggleActive("projects")}><a href="#projects" className="nav__link">Projects</a></li>
+                    <li className={[classes.contact.join(" ")]} onClick={() => toggleActive("contact")}><a href="#contact" className="nav__link">Contact Me</a></li>
+                </ul>
+
+                <div className="copyright">&copy; Copyright 2020 by Wisam Mozalbat</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
