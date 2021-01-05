aa278861af5361480eb705cd839c7bd13aa74079
Wisam <wisam_mozalbat@hotmail.com>
2021-01-05 19:00:25 -0400

    Added art gallery

--- src/pages/Projects/ProjectItems/ProjectItems.js
+++ src/pages/Projects/ProjectItems/ProjectItems.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProjectItem from '../ProjectItem/ProjectItem'
 import memoryGame from '../../../assets/memory-game.png'
+import artGallery from '../../../assets/art-gallery.png'
 
 import './ProjectItems.css'
 
 const items = [
-    {src: memoryGame, alt: 'Memory game', name: 'Memory game', description: 'Memory game made in React with the use of CSS3', 
-    techs: ['React', 'CSS3', 'Responsive'], links:{github: 'https://github.com/Wisammozalbat/wamt-memory-game.git', demo: 'https://ecstatic-boyd-b2674a.netlify.app/'}}
+    {src: memoryGame, alt: 'Memory game', name: 'Memory game', description: 'Memory game made in React with the use of HTML5, Javascript and CSS3', 
+    techs: ['React', 'CSS3', 'Responsive'], links:{github: 'https://github.com/Wisammozalbat/wamt-memory-game.git', demo: 'https://ecstatic-boyd-b2674a.netlify.app/'}},
+    {src: artGallery, alt: 'Art Gallery', name: 'TrendArt', description: 'Art Gallery made in React with the use of HTML5, Javascript and CSS3', 
+    techs: ['React', 'CSS3', 'HTML5'], links:{github: 'https://github.com/Wisammozalbat/art-gallery.git', demo: 'https://wisammozalbat.github.io/art-gallery/'}},
 ]
 
 const ProjectItems = props => {
