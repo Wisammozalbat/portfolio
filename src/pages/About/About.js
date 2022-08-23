4ca907afd80ed36bf3ee7730fb48a46d57ac6994
Wisam <wisam_mozalbat@hotmail.com>
2022-08-23 19:23:51 -0400

    update resume

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import './About.css';
+import React from 'react'
+import './About.css'
 
 import { FaRegEye } from 'react-icons/fa'
 
-const About = props => {
-    return (
-        <div className="about__container">
-            <div className="about__name heading-1 heading-1--light">Wisam Mozalbat</div>
-            <div className="about__text"><span style={{animation: props.isVisible ? 'bgMove 65s linear infinite' : 'none'}}>I'm a Computer Engineer focused on Front-End development. I'm passionate about my work and I'm always looking to improve my skills and find new challenges.</span></div>
-            <div className="about__resume"><FaRegEye className='resume-icon'/><a className='about__resume-link link' href="https://drive.google.com/file/d/1oWcdmVB6Xd0KMXgsPyBO_EBZG-k86qLi/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></div>
-        </div>
-    )
+const About = (props) => {
+  return (
+    <div className="about__container">
+      <div className="about__name heading-1 heading-1--light">
+        Wisam Mozalbat
+      </div>
+      <div className="about__text">
+        <span
+          style={{
+            animation: props.isVisible ? 'bgMove 65s linear infinite' : 'none',
+          }}
+        >
+          I'm a Computer Engineer focused on Front-End development. I'm
+          passionate about my work and I'm always looking to improve my skills
+          and find new challenges.
+        </span>
+      </div>
+      <div className="about__resume">
+        <FaRegEye className="resume-icon" />
+        <a
+          className="about__resume-link link"
+          href="https://drive.google.com/file/d/10BNXZ4eGmx9PvHzJMZBRR3sQVI6d4H-X/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Resume
+        </a>
+      </div>
+    </div>
+  )
 }
 
-export default About;
+export default About
