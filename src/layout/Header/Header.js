64365c6c9244386588e5bbaca955279638889900
Wisam <wisam_mozalbat@hotmail.com>
2022-08-23 19:16:25 -0400

    updated email

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,78 +1,133 @@
-import React, { useEffect, useState } from 'react';
-import './Header.css';
+import React, { useEffect, useState } from 'react'
+import './Header.css'
 
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
-import { SiUpwork } from "react-icons/si"
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import { SiUpwork } from 'react-icons/si'
 
-import {ReactComponent as AppLogo} from '../../assets/logo-wm.svg';
+import { ReactComponent as AppLogo } from '../../assets/logo-wm.svg'
 
 const defaultClasses = {
+  about: ['nav__item'],
+  technologies: ['nav__item'],
+  projects: ['nav__item'],
+  contact: ['nav__item'],
+}
+
+const Header = (props) => {
+  const [classes, setClasses] = useState({
     about: ['nav__item'],
     technologies: ['nav__item'],
     projects: ['nav__item'],
-    contact: ['nav__item']
-}
+    contact: ['nav__item'],
+  })
 
-const Header = props => {
+  useEffect(() => {
+    toggleActive(props.activeLink)
+  }, [props.activeLink])
 
-    const [classes, setClasses] = useState({
-        about: ['nav__item'],
-        technologies: ['nav__item'],
-        projects: ['nav__item'],
-        contact: ['nav__item']
-    });
-
-    useEffect(() => {
-        toggleActive(props.activeLink);
-    }, [props.activeLink])
-
-    const toggleActive = (activeItem) => {
-        let newStyleClasses = {...defaultClasses}
-            for (let item in newStyleClasses) {
-                if (item === activeItem) {
-                    newStyleClasses[item] = [...defaultClasses[item], 'active'];
-                } else {
-                    newStyleClasses[item] = [...defaultClasses[item]]
-                }
-            }
-        setClasses({...newStyleClasses});
+  const toggleActive = (activeItem) => {
+    let newStyleClasses = { ...defaultClasses }
+    for (let item in newStyleClasses) {
+      if (item === activeItem) {
+        newStyleClasses[item] = [...defaultClasses[item], 'active']
+      } else {
+        newStyleClasses[item] = [...defaultClasses[item]]
+      }
     }
+    setClasses({ ...newStyleClasses })
+  }
 
-    const [burgerActive, setBurgerActive] = useState(false)
+  const [burgerActive, setBurgerActive] = useState(false)
 
-    return (
-        <div className="header__container">
-            <div className="header__content">
-                <a href="/"><AppLogo className="header__logo"/></a>
+  return (
+    <div className="header__container">
+      <div className="header__content">
+        <a href="/portfolio">
+          <AppLogo className="header__logo" />
+        </a>
 
-                <div className={['burger', burgerActive ? 'active' : ''].join(" ")} onClick={() => setBurgerActive(curr => !curr)}>
-                    <div className="burg1"></div>
-                    <div className="burg2"></div>
-                    <div className="burg3"></div>
-                </div>
-
-                <ul className={['nav', burgerActive ? 'active' : ''].join(" ")}>
-                    <li className={[classes.about.join(" ")]} onClick={() => {toggleActive("about"); setBurgerActive(false)}}>
-                        <a href="#about" className="nav__link">About Me</a></li>
-                    <li className={[classes.technologies.join(" ")]} onClick={() => {toggleActive("technologies"); setBurgerActive(false)}}><a href="#technologies" className="nav__link">Technologies</a></li>
-                    <li className={[classes.projects.join(" ")]} onClick={() =>{toggleActive("projects"); setBurgerActive(false)}}><a href="#projects" className="nav__link">Projects</a></li>
-                    <li className={[classes.contact.join(" ")]} onClick={() => {toggleActive("contact"); setBurgerActive(false)}}><a href="#contact" className="nav__link">Contact Me</a></li>
-                </ul>
-
-                <div className="social-media">
-                    <a href="https://github.com/Wisammozalbat" target="_blank" rel="noreferrer">
-                        <AiFillGithub className="social-media__icon" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/wisam-mozalbat/" target="_blank" rel="noreferrer">
-                        <AiFillLinkedin className="social-media__icon"/>
-                    </a>
-                    <a href="https://www.upwork.com/o/profiles/users/~01904da89701faa749/" target="_blank" rel="noreferrer">
-                        <SiUpwork className="social-media__icon"/>
-                    </a>
-                </div>
-            </div>
+        <div
+          className={['burger', burgerActive ? 'active' : ''].join(' ')}
+          onClick={() => setBurgerActive((curr) => !curr)}
+        >
+          <div className="burg1"></div>
+          <div className="burg2"></div>
+          <div className="burg3"></div>
         </div>
-    )
+
+        <ul className={['nav', burgerActive ? 'active' : ''].join(' ')}>
+          <li
+            className={[classes.about.join(' ')]}
+            onClick={() => {
+              toggleActive('about')
+              setBurgerActive(false)
+            }}
+          >
+            <a href="#about" className="nav__link">
+              About Me
+            </a>
+          </li>
+          <li
+            className={[classes.technologies.join(' ')]}
+            onClick={() => {
+              toggleActive('technologies')
+              setBurgerActive(false)
+            }}
+          >
+            <a href="#technologies" className="nav__link">
+              Technologies
+            </a>
+          </li>
+          <li
+            className={[classes.projects.join(' ')]}
+            onClick={() => {
+              toggleActive('projects')
+              setBurgerActive(false)
+            }}
+          >
+            <a href="#projects" className="nav__link">
+              Projects
+            </a>
+          </li>
+          <li
+            className={[classes.contact.join(' ')]}
+            onClick={() => {
+              toggleActive('contact')
+              setBurgerActive(false)
+            }}
+          >
+            <a href="#contact" className="nav__link">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+
+        <div className="social-media">
+          <a
+            href="https://github.com/Wisammozalbat"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className="social-media__icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/wisam-mozalbat/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin className="social-media__icon" />
+          </a>
+          <a
+            href="https://www.upwork.com/o/profiles/users/~01904da89701faa749/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiUpwork className="social-media__icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
--- src/pages/Projects/ProjectItems/ProjectItems.js
+++ src/pages/Projects/ProjectItems/ProjectItems.js
@@ -1,28 +1,72 @@
-import React from 'react';
+import React from 'react'
 import ProjectItem from '../ProjectItem/ProjectItem'
 import memoryGame from '../../../assets/memory-game.png'
 import artGallery from '../../../assets/art-gallery.png'
+import conintech from '../../../assets/conintech.png'
 
 import './ProjectItems.css'
 
 const items = [
-    {src: memoryGame, alt: 'Memory game', name: 'Memory game', description: 'Memory game made in React with the use of HTML5, Javascript and CSS3', 
-    techs: ['React', 'CSS3', 'Responsive'], links:{github: 'https://github.com/Wisammozalbat/wamt-memory-game.git', demo: 'https://ecstatic-boyd-b2674a.netlify.app/'}},
-    {src: artGallery, alt: 'Art Gallery', name: 'TrendArt', description: 'Art Gallery made in React with the use of HTML5, Javascript and CSS3', 
-    techs: ['React', 'CSS3', 'HTML5'], links:{github: 'https://github.com/Wisammozalbat/art-gallery.git', demo: 'https://wisammozalbat.github.io/art-gallery/'}},
+  {
+    src: memoryGame,
+    alt: 'Memory game',
+    name: 'Memory game',
+    description:
+      'Memory game made in React with the use of JSX, Javascript and CSS3',
+    techs: ['React', 'CSS3', 'Responsive'],
+    links: {
+      github: 'https://github.com/Wisammozalbat/wamt-memory-game.git',
+      demo: 'https://ecstatic-boyd-b2674a.netlify.app/',
+    },
+  },
+  {
+    src: artGallery,
+    alt: 'Art Gallery',
+    name: 'TrendArt',
+    description:
+      'Art Gallery made in React with the use of JSX, Javascript and CSS3',
+    techs: ['React', 'CSS3', 'HTML5'],
+    links: {
+      github: 'https://github.com/Wisammozalbat/art-gallery.git',
+      demo: 'https://wisammozalbat.github.io/art-gallery/',
+    },
+  },
+  {
+    src: conintech,
+    alt: 'Conintech Home',
+    name: 'Conintech',
+    description:
+      'Lading for Conintech, made in React with the use of JSX, Javascript and CSS3',
+    techs: ['React', 'CSS3', 'HTML5'],
+    links: {
+      github: 'https://github.com/Wisammozalbat/conintech',
+      demo: 'https://conintech.net/',
+    },
+  },
 ]
 
-const ProjectItems = props => {
-    return (
-        <div className="projects__container">
-            <h2 className="heading-1 heading-1--light heading-1-bg--square">Projects</h2>
-            <div className="projects__content">
-                {items.map((item, i) => <ProjectItem key={i} img={item.src} alt={item.alt} name={item.name} description={item.description} 
-                                                    techs={item.techs} links={item.links}/>)}
-            </div>
-            <p className="more-soon">More in progress...</p>
-        </div>
-    );
+const ProjectItems = (props) => {
+  return (
+    <div className="projects__container">
+      <h2 className="heading-1 heading-1--light heading-1-bg--square">
+        Projects
+      </h2>
+      <div className="projects__content">
+        {items.map((item, i) => (
+          <ProjectItem
+            key={i}
+            img={item.src}
+            alt={item.alt}
+            name={item.name}
+            description={item.description}
+            techs={item.techs}
+            links={item.links}
+          />
+        ))}
+      </div>
+      <p className="more-soon">More in progress...</p>
+    </div>
+  )
 }
 
-export default ProjectItems;
+export default ProjectItems
